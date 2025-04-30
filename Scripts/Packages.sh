9c64eb7db57d7c21ac5ed6a3048d8322a7b008be
Gudark <62348439+Gudark@users.noreply.github.com>
2025-05-01 00:51:21 +0800

    Update Packages.sh
    homeproxy更换为clash api版本

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -48,7 +48,10 @@ UPDATE_PACKAGE() {
 UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
-UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+# homeproxy-with-clash-api
+# UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+UPDATE_PACKAGE "homeproxy" "kenzok8/small-package" "main" "pkg"
+
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
